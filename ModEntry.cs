60c944a99c19a8ad567788970c13b6ca8cac3182
Jeremy Grozavescu <jgrozavescu@gmail.com>
2022-07-25 22:08:56 -0500

    Initial Commit

---
+++ ModEntry.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using StardewModdingAPI;
+using StardewModdingAPI.Events;
+using SpaceCore.Events;
+
+
+namespace Lanturnalis.Stardew.Starlight
+{
+
+    public class ModEntry : Mod
+    {
+
+        public override void Entry(IModHelper helper)
+        {
+            
+            Helper.Events.Input.ButtonPressed += this.OnDebugPressed;
+            SpaceEvents.OnBlankSave += this.OnBlankSave;
+        }
+
+        private void OnBlankSave(object sender, EventArgs e) {
+            this.Monitor.Log("OnBlankSave Detected");
+        }
+        
+        private void OnDebugPressed(object sender, ButtonPressedEventArgs e)
+        {
+            if(e.Button == SButton.F10) {
+                this.Monitor.Log("Debug button pressed");
+            }
+        }
+    }
+}
